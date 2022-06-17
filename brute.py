b60ff2282a27668fe75ed4b1a0a4d80fd2938513
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2022-06-17 09:22:20 +0200

    working

--- brute.py
+++ brute.py
@@ -2,6 +2,7 @@ import sys
 
 import networkx as nx
 from networkx import grid_graph
+from networkx.relabel import convert_node_labels_to_integers
 import matplotlib.pyplot as plt
 
 
@@ -12,13 +13,26 @@ def hamiltonians(G, vis = []):
                 yield p
     else:
         dests = set(G[vis[-1]]) - set(vis)
-        if not dests and len(vis) == len(G):
+        if not dests and len(vis) == len(G.nodes()):
             yield vis
         for n in dests:
             for p in hamiltonians(G, vis + [n]):
                 yield p
 
+def lattice_draw(lattice, walk):
+    steps = [(walk[i], walk[i+1]) for i in range(0, len(walk)-1)]
+
+    pos = nx.spring_layout(lattice)
+    nx.draw_networkx_nodes(lattice, pos)
+    nx.draw_networkx_edges(lattice, pos, edgelist=steps)
+    plt.show()
+    pass
 if __name__ == "__main__":
-    n = int(sys.argv[1])
+    # n = int(sys.argv[1])
+    n = 4
     lattice = grid_graph(dim=(n, n))
-    paths = list(hamiltonians(lattice))
+    lattice = convert_node_labels_to_integers(lattice)
+    ps = list(hamiltonians(lattice))
+    print(len(ps))
+    for path in ps:
+        lattice_draw(lattice, path)
